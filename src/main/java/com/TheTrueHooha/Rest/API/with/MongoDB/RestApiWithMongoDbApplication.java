c856cacb077967bd82d5f2a38b37873a9f940c6f
David Ogar <owogogahhero@outlook.com>
2022-02-22 08:47:12 +0100

    starter project

---
+++ src/main/java/com/TheTrueHooha/Rest/API/with/MongoDB/RestApiWithMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.TheTrueHooha.Rest.API.with.MongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWithMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiWithMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TheTrueHooha/Rest/API/with/MongoDB/RestApiWithMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TheTrueHooha.Rest.API.with.MongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiWithMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
