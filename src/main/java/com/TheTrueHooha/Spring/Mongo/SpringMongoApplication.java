4b6b47babbe39754c0141e929ff3145ef11da90a
David Ogar <owogogahhero@outlook.com>
2022-03-12 08:35:18 +0100

    adding files to remote

---
+++ src/main/java/com/TheTrueHooha/Spring/Mongo/SpringMongoApplication.java
@@ -0,0 +1,13 @@
+package com.TheTrueHooha.Spring.Mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TheTrueHooha/Spring/Mongo/SpringMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TheTrueHooha.Spring.Mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
